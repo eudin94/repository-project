e9ef060e89dc83ec4b686dc924dd4dba3930b96a
Eduardo Comerlato <eduardo.comerlato@southsystem.com.br>
2023-07-11 21:48:22 -0300

    Implementing command line runner

--- src/main/java/br/edu/ifrs/restinga/comerlato/repositoryproject/Application.java
+++ src/main/java/br/edu/ifrs/restinga/comerlato/repositoryproject/Application.java
@@ -1,62 +1,47 @@
 package br.edu.ifrs.restinga.comerlato.repositoryproject;
 
+import br.edu.ifrs.restinga.comerlato.repositoryproject.model.entity.onetoone.bidirectional.PassportBi;
+import br.edu.ifrs.restinga.comerlato.repositoryproject.model.entity.onetoone.bidirectional.PersonBi;
+import br.edu.ifrs.restinga.comerlato.repositoryproject.repository.onetoone.bidirectional.PersonRepositoryBi;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static java.time.LocalDate.now;
 
 @Slf4j
 @SpringBootApplication
 public class Application {
 
-//    @Autowired
-//    private PersonRepository repository;
+    @Autowired
+    private PersonRepositoryBi personRepositoryBi;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-//    @Bean
-//    public CommandLineRunner initialData() {
-//
-//        return (args -> {
-//
-//            final var person1 = Person.builder()
-//                    .cpf("12345678901")
-//                    .name("Eduardo 1")
-//                    .email("eduardo1@mail.com")
-//                    .build();
-//
-//            log.info("CREATED PERSON 1");
-//
-//            final var saved1 = repository.save(person1);
-//            log.info("SAVED = " + saved1);
-//
-//            final var person2 = Person.builder()
-//                    .cpf("12345678902")
-//                    .name("Eduardo 2")
-//                    .email("eduardo2@mail.com")
-//                    .build();
-//
-//            final var saved2 = repository.save(person2);
-//            log.info("SAVED = " + saved2);
-//
-//            final var person3 = Person.builder()
-//                    .cpf("12345678903")
-//                    .name("Eduardo 3")
-//                    .email("eduardo3@mail.com")
-//                    .build();
-//
-//            final var saved3 = repository.save(person3);
-//            log.info("SAVED = " + saved3);
-//
-//            repository.delete(saved1);
-//            log.warn("DELETED = " + saved1);
-//
-//            repository.delete(saved2);
-//            log.warn("DELETED = " + saved2);
-//
-//            repository.delete(saved3);
-//            log.warn("DELETED = " + saved3);
-//        });
-//    }
+    @Bean
+    public CommandLineRunner initialData() {
+
+        return (args -> {
+
+            final var passport = PassportBi.builder()
+                    .number("12345")
+                    .issueDate(now())
+                    .expirationDate(now())
+                    .build();
+
+            final var person = PersonBi.builder()
+                    .name("Eduardo 1")
+                    .birthdate(now())
+                    .cpf("12345678901")
+                    .passport(passport)
+                    .build();
+
+            personRepositoryBi.save(person);
+        });
+    }
 }
